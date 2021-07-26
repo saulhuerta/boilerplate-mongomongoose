d300a96850e287dd30e6d2dc6c05f81517bbe13f
saul.huerta <saul.huerta@unosquare.com>
2021-07-25 22:51:40 -0500

    Use findByIdAndRemove

--- myApp.js
+++ myApp.js
@@ -98,7 +98,12 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  
+  Person.findByIdAndRemove( personId, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
+
 };
 
 const removeManyPeople = (done) => {
